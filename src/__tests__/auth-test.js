66d437de6d3c24af66f73b0aa62dbb65730ed69a
Andrew Ochsner <andrew.ochsner@wipro.com>
2016-09-20 16:31:54 -0500

    and this one...
    NETWRKDIAG-121

--- src/__tests__/auth-test.js
+++ src/__tests__/auth-test.js
@@ -1,6 +1,6 @@
 /* eslint no-unused-expressions: 0 */
 const expect = require('chai').expect;
-//const Auth = require('../auth');
+// const Auth = require('../auth');
 
 describe('Auth', () => {
   it('logs in', () => {
