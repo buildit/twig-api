9b6d4ebdfefb632c23ac7977dc2cadd343bc7eb4
Andrew Ochsner <andrew.ochsner@wipro.com>
2016-09-20 22:50:27 -0500

    Adding blocks around stage sections
    NETWRKDIAG-121

--- pipelines/production.groovy
+++ pipelines/production.groovy
@@ -6,7 +6,7 @@ node {
 
   try {
 
-    stage "Set Up"
+    stage "Set Up" {
       checkout scm
 
       sh "curl -L https://dl.bintray.com/buildit/maven/jenkins-pipeline-libraries-${env.PIPELINE_LIBS_VERSION}.zip -o lib.zip && echo 'A' | unzip lib.zip"
@@ -26,16 +26,18 @@ node {
       gitUrl = "https://bitbucket.org/digitalrigbitbucketteam/twig-api"
       // clean the workspace before checking out
       sh "git clean -ffdx"
+    }
 
-    stage "Write docker-compose"
+    stage "Write docker-compose" {
       // global for exception handling
       tag = ui.selectTag(ecr.imageTags(appName, env.AWS_REGION))
       def tmpFile = UUID.randomUUID().toString() + ".tmp"
       def ymlData = template.transform(readFile("docker-compose.yml.template"), [tag :tag, registryBase :registryBase])
 
       writeFile(file: tmpFile, text: ymlData)
+    }
 
-    stage "Deploy to production"
+    stage "Deploy to production" {
       sh "convox login ${env.CONVOX_RACKNAME} --password ${env.CONVOX_PASSWORD}"
       sh "convox deploy --app ${appName} --description '${tag}' --file ${tmpFile}"
       sh "rm ${tmpFile}"
@@ -43,6 +45,7 @@ node {
       convox.waitUntilDeployed("${appName}")
       convox.ensureSecurityGroupSet("${appName}", env.CONVOX_SECURITYGROUP)
       slack.notify("Deployed to Production", "Tag '<${gitUrl}/commits/tag/${tag}|${tag}>' has been deployed to <${appUrl}|${appUrl}>", "good", "http://i296.photobucket.com/albums/mm200/kingzain/the_eye_of_sauron_by_stirzocular-d86f0oo_zpslnqbwhv2.png", slackChannel)
+    }
   }
   catch (err) {
     currentBuild.result = "FAILURE"
--- pipelines/staging.groovy
+++ pipelines/staging.groovy
@@ -3,7 +3,7 @@ node {
     currentBuild.result = "SUCCESS"
 
     try {
-      stage "Set Up"
+      stage "Set Up" {
         sh "curl -L https://dl.bintray.com/buildit/maven/jenkins-pipeline-libraries-${env.PIPELINE_LIBS_VERSION}.zip -o lib.zip && echo 'A' | unzip -o lib.zip"
 
         ecr = load "lib/ecr.groovy"
@@ -24,19 +24,22 @@ node {
         gitUrl = "https://bitbucket.org/digitalrigbitbucketteam/twig-api"
         // clean the workspace before checking out
         sh "git clean -ffdx"
+      }
 
-      stage "Checkout"
+      stage "Checkout" {
         checkout scm
 
         // global for exception handling
         shortCommitHash = git.getShortCommit()
         def commitMessage = git.getCommitMessage()
         def version = npm.getVersion()
+      }
 
-      stage "Install"
+      stage "Install" {
         sh "npm install"
+      }
 
-      stage "Test"
+      stage "Test" {
         try {
           sh "npm run test:ci"
         }
@@ -44,27 +47,32 @@ node {
           junit 'reports/test-results.xml'
         }
         publishHTML(target: [reportDir: 'reports/lcov-report', reportFiles: 'index.html', reportName: 'Coverage Results'])
+      }
 
-      stage "Analysis"
+      stage "Analysis" {
         sh "npm run lint"
         sh "npm run validate"
         sh "npm run security"
         // sh "/usr/local/sonar-scanner-2.6.1/bin/sonar-scanner -e -Dsonar.projectVersion=${version}"
+      }
 
-      stage "Package"
+      stage "Package" {
         sh "npm shrinkwrap"
+      }
 
-      stage "Docker Image Build"
+      stage "Docker Image Build" {
         def tag = "${version}-${shortCommitHash}-${env.BUILD_NUMBER}"
         def image = docker.build("${appName}:${tag}", '.')
         ecr.authenticate(env.AWS_REGION)
+      }
 
-      stage "Docker Push"
+      stage "Docker Push" {
         docker.withRegistry(registry) {
           image.push("${tag}")
         }
+      }
 
-      stage "Deploy To AWS"
+      stage "Deploy To AWS" {
         def tmpFile = UUID.randomUUID().toString() + ".tmp"
         def ymlData = template.transform(readFile("docker-compose.yml.template"), [tag :tag, registryBase :registryBase])
         writeFile(file: tmpFile, text: ymlData)
@@ -74,14 +82,18 @@ node {
         // wait until the app is deployed
         convox.waitUntilDeployed("${appName}-staging")
         convox.ensureSecurityGroupSet("${appName}-staging", env.CONVOX_SECURITYGROUP)
+      }
 
-      stage "Run Functional Tests"
+      stage "Run Functional Tests" {
 
-      stage "Promote Build to latest"
+      }
+
+      stage "Promote Build to latest" {
         docker.withRegistry(registry) {
           image.push("latest")
         }
         slack.notify("Deployed to Staging", "Commit '<${gitUrl}/commits/${shortCommitHash}|${shortCommitHash}>' has been deployed to <${appUrl}|${appUrl}>\n\n${commitMessage}", "good", "http://i296.photobucket.com/albums/mm200/kingzain/the_eye_of_sauron_by_stirzocular-d86f0oo_zpslnqbwhv2.png", slackChannel)
+      }
     }
     catch (err) {
       currentBuild.result = "FAILURE"
