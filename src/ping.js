dbe9b5fe8d858198fe09c7a264331d598d5d4303
Andrew Ochsner <andrew.ochsner@wipro.com>
2016-09-27 13:56:44 -0500

    Add version to ping
    NETWRKDIAG-121

--- src/ping.js
+++ src/ping.js
@@ -1,6 +1,9 @@
 const config = require('./utils/config');
 const ramda = require('ramda');
+const version = require('../package').version;
 
-module.exports.ping = (request, reply) => {
-  return reply({ config: ramda.omit(config._secrets)(config) });
-};
+module.exports.ping = (request, reply) =>
+  reply({
+    version,
+    config: ramda.omit(config._secrets)(config)
+  });
