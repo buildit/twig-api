2b365177030dd047530c59a848d9413d853b9432
rsafronov <electroma@gmail.com>
2016-11-21 15:07:15 -0500

    Changed prod pipeline to use global library

--- pipelines/production.groovy
+++ pipelines/production.groovy
@@ -1,4 +1,6 @@
 // Production release pipeline
+@Library('buildit@master')
+def VERSION='master'
 
 node {
 
@@ -11,13 +13,11 @@ node {
       // clean the workspace before checking out
       sh "git clean -ffdx"
 
-      sh "curl -L https://dl.bintray.com/buildit/maven/jenkins-pipeline-libraries-${env.PIPELINE_LIBS_VERSION}.zip -o lib.zip && echo 'A' | unzip lib.zip"
-
-      ui = load "lib/ui.groovy"
-      ecr = load "lib/ecr.groovy"
-      slack = load "lib/slack.groovy"
-      template = load "lib/template.groovy"
-      convox = load "lib/convox.groovy"
+      uiInst = new ui()
+      ecrInst = new ecr()
+      slackInst = new slack()
+      templateInst = new template()
+      convox = new convox()
 
       appName = "twig-api"
       registryBase = "006393696278.dkr.ecr.${env.AWS_REGION}.amazonaws.com"
@@ -26,12 +26,14 @@ node {
       gitUrl = "https://bitbucket.org/digitalrigbitbucketteam/twig-api"
       tmpFile = UUID.randomUUID().toString() + ".tmp"
 
+      sendNotifications = !env.DEV_MODE
+
       // select the tag
-      tag = ui.selectTag(ecr.imageTags(appName, env.AWS_REGION))
+      tag = ui.selectTag(ecrInst.imageTags(appName, env.AWS_REGION))
     }
 
     stage("Write docker-compose") {
-      def ymlData = template.transform(readFile("docker-compose.yml.template"), [tag :tag, registryBase :registryBase])
+      def ymlData = templateInst.transform(readFile("docker-compose.yml.template"), [tag :tag, registryBase :registryBase])
 
       writeFile(file: tmpFile, text: ymlData)
     }
@@ -41,14 +43,14 @@ node {
       sh "convox deploy --app ${appName} --description '${tag}' --file ${tmpFile} --wait"
       sh "rm ${tmpFile}"
       // wait until the app is deployed
-      convox.waitUntilDeployed("${appName}")
+      convoxInst.waitUntilDeployed("${appName}")
       convox.ensureSecurityGroupSet("${appName}", env.CONVOX_SECURITYGROUP)
-      slack.notify("Deployed to Production", "Tag '<${gitUrl}/commits/tag/${tag}|${tag}>' has been deployed to <${appUrl}|${appUrl}>", "good", "http://i296.photobucket.com/albums/mm200/kingzain/the_eye_of_sauron_by_stirzocular-d86f0oo_zpslnqbwhv2.png", slackChannel)
+      if(sendNotifications) slackInst.notify("Deployed to Production", "Tag '<${gitUrl}/commits/tag/${tag}|${tag}>' has been deployed to <${appUrl}|${appUrl}>", "good", "http://i296.photobucket.com/albums/mm200/kingzain/the_eye_of_sauron_by_stirzocular-d86f0oo_zpslnqbwhv2.png", slackChannel)
     }
   }
   catch (err) {
     currentBuild.result = "FAILURE"
-    slack.notify("Error while deploying to Production", "Tag '<${gitUrl}/commits/tag/${tag}|${tag}>' failed to deploy to <${appUrl}|${appUrl}>", "danger", "http://i296.photobucket.com/albums/mm200/kingzain/the_eye_of_sauron_by_stirzocular-d86f0oo_zpslnqbwhv2.png", slackChannel)
+    if(sendNotifications) slackInst.notify("Error while deploying to Production", "Tag '<${gitUrl}/commits/tag/${tag}|${tag}>' failed to deploy to <${appUrl}|${appUrl}>", "danger", "http://i296.photobucket.com/albums/mm200/kingzain/the_eye_of_sauron_by_stirzocular-d86f0oo_zpslnqbwhv2.png", slackChannel)
     throw err
   }
 }
