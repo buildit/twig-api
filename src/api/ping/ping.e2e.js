1aa287825125552da9eb47974c3f29efc32e57a8
Andrew Ochsner <andrew.ochsner@wipro.com>
2016-12-20 13:45:11 -0600

    Add basic acceptance test
    refactor code coverage

---
+++ src/api/ping/ping.e2e.js
@@ -0,0 +1,26 @@
+/* eslint func-names: 0 */
+/* eslint no-unused-expressions: 0 */
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const version = require('../../../package').version;
+const anonAgent = require('../../../test/e2e').anonAgent;
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+describe('/Ping', () => {
+  describe('GET', () => {
+    let res;
+
+    before(function* () {
+      res = yield anonAgent.get('/ping');
+    });
+
+    it('returns 200', () => {
+      expect(res).to.have.status(200);
+    });
+
+    it('returns the version', () => {
+      expect(res.body.version).to.eq(version);
+    });
+  });
+});
---
+++ test/e2e/index.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+const url = process.env.URL || 'http://localhost:3000';
+
+const authAgent = chai.request.agent(url);
+const anonAgent = chai.request.agent(url);
+
+module.exports = { authAgent, anonAgent };
