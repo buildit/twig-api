a526bbccf25a28309b7f2e1d48faa8b5174383a2
Ben Hernandez <ben.hernandez@wipro.com>
2017-05-18 13:57:13 -0600

    3.2.0

--- src/api/v2/ping/ping.e2e.js
+++ src/api/v2/ping/ping.e2e.js
@@ -3,7 +3,6 @@
 /* eslint no-unused-expressions: 0 */
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const version = require('../../../../package').version;
 const anonAgent = require('../../../../test/e2e').anonAgent;
 
 const expect = chai.expect;
@@ -20,10 +19,6 @@ describe('/v2/ping', () => {
 
     it('returns 200', () => {
       expect(res).to.have.status(200);
-    });
-
-    it('returns the version', () => {
-      expect(res.body.version).to.eq(version);
     });
   });
 });
