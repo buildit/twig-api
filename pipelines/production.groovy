1b509b69be8ac867d23cfe3877b858b2ea2c98a6
Andrew Ochsner <andrew.ochsner@wipro.com>
2017-06-09 09:50:16 -0500

    point appurl to buildit.tools

--- pipelines/production.groovy
+++ pipelines/production.groovy
@@ -33,7 +33,7 @@ node {
 
       appName = "twig-api"
       registryBase = "006393696278.dkr.ecr.${env.AWS_REGION}.amazonaws.com"
-      appUrl = "http://twig-api.riglet"
+      appUrl = "https://twig-api.buildit.tools"
       slackChannel = "twig"
       gitUrl = "https://bitbucket.org/digitalrigbitbucketteam/twig-api"
       tmpFile = UUID.randomUUID().toString() + ".tmp"
