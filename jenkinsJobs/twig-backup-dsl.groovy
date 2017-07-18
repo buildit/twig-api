3f2aff1cdb9db272b5167a38609aa73e8c80bcc7
Andrew Ochsner <andrew.ochsner@wipro.com>
2017-07-18 12:14:27 -0500

    new name for now

--- jenkinsJobs/twig-backup-dsl.groovy
+++ jenkinsJobs/twig-backup-dsl.groovy
@@ -1,4 +1,4 @@
-pipelineJob('twig-backup') {
+pipelineJob('twig-backup-andytest') {
   description('Nightly backups of CouchDB&apos;s onto S3 at https://console.aws.amazon.com/s3/home?region=us-west-2#&amp;bucket=twig-backups')
   definition {
     cpsScm {
