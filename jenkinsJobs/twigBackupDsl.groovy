04e48e7c6fbd60e738b91d24c2d92531259b9673
Andrew Ochsner <andrew.ochsner@wipro.com>
2017-07-18 13:52:48 -0500

    WAG #2

--- jenkinsJobs/twigBackupDsl.groovy
+++ jenkinsJobs/twigBackupDsl.groovy
@@ -2,7 +2,7 @@ pipelineJob('twig-backup-andytest') {
   description("Nightly backups of CouchDB's onto S3 at https://console.aws.amazon.com/s3/home?region=us-west-2#&bucket=twig-backups")
   definition {
     cpsScm {
-      scm(SEED_JOB.cpsScm.scm)
+      scm(SEED_JOB.typicalScm)
       scriptPath('pipelines/twig-backup.groovy')
     }
   }
